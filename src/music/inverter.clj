d8e6f660510e58a32c912ba096b64381d79d09df
Paul Sanwald <pcsanwald@gmail.com>
2012-05-06 19:06:01 -0400

    adding doc strings

--- src/music/inverter.clj
+++ src/music/inverter.clj
@@ -1,6 +1,9 @@
 (ns music.inverter)
 
 (defn step
+    "a stepper, which pops off the first item and adds it to
+    the end. there's probably something in clojure.core that
+    does this, should investigate more."
     ([coll]
     (conj (vec (rest coll)) (first coll)))
     ([coll steps]
@@ -12,6 +15,7 @@
 (defmulti transposer (fn [_ thing] (class thing)))
 
 (defmethod transposer Long 
+    "transpose a scale to a given degree, from 0 to (count scale)"
     [scale degree]
     (loop [new-scale scale count degree]
     (if (zero? count)
@@ -19,6 +23,7 @@
         (recur (step new-scale) (dec count)))))
 
 (defmethod transposer clojure.lang.Keyword 
+    "transpose a scale by an item in the scale" 
     [scale note]
     (loop [new-scale scale]
     (if (= (first new-scale) note)
@@ -26,6 +31,10 @@
         (recur (step new-scale)))))
 
 (defn scale-stepper
+    "step through a scale given an intervallic recipe.
+    the recipe should be an intervallic sequence:
+    common tone, second, second would be expressed as [0 1 1].
+    "
     ([scale recipe]
     (lazy-seq 
         (cons 
@@ -44,11 +53,16 @@
     ))))
 
 (defn make-scale
+    "make a scale using a voicing, scale and degree.
+    this is really an implementation detail and probably
+    not directly useful."
     [recipe voicing scale degree]
     (take (count scale) 
     (scale-stepper (transposer scale (nth voicing degree)) (step recipe degree))))
 
 (defn inverter
+    "create a chord scale given an intervallic recipe, spelling of chord,
+    and the scale to use"
     [recipe spelling scale]
     (let
     [voicing (map dec spelling)
